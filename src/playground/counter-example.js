bfdc2238ed2ea2bee3633e439e51b15fb2ccf948
Delwyn <Delwyn@Delwyns-MacBook-Air.local>
2019-04-22 12:55:41 -0400

    implemented Default Prop Values to counter

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne=this.handleMinusOne.bind(this);
         this.handleReset=this.handleReset.bind(this);
         this.state = {
-            count: 0,
+            count: props.count
 
         };
     }
@@ -30,11 +30,7 @@ class Counter extends React.Component {
                 count:0
             };
         });
-        this.setState((prevState)=> {
-            return {
-                count: prevState.count +1
-            };
-        });
+
         // this.setState({
         //     count:0
         // });
@@ -56,43 +52,12 @@ class Counter extends React.Component {
     }
 }
 
+Counter.defaultProps={
+    count: 0
+}
+
 ReactDOM.render(<Counter />,document.getElementById('app'));
 
 
 
 
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count=0;
-//     renderCounterApp();
-// };
-
-
-
-// const appRoot = document.getElementById('app');
-
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>Reset</button>
-    
-//         </div>
-//     );
-
-//     ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
