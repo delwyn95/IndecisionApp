b2f58b7462f7ea2ac8bd72f1ee19b769c8b9acc7
Delwyn <Delwyn@delwyns-air.lan>
2019-04-23 21:27:20 -0400

    implemented ES6 class properties for state declaration and event handlers

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,17 +6,37 @@ import Header from './Header';
 import Action from './Action';
 
 class IndecisionApp extends React.Component {
-    constructor(props){
-        super(props);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleDeleteOptions=this.handleDeleteOptions.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: []
-        };
-    }
-    componentDidMount() {
+    state = {
+        options:[]
+    };
+
+    handleDeleteOptions = () => {
+        this.setState(() => ( { options:[] } ));
+    };
+    // Setting it to False removes. Setting true, keeps it
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState)=> ({
+            options: prevState.options.filter((option)=> !(optionToRemove === option))
+        }))
+    };
+    handlePick = () => {
+        const randNum = Math.floor(Math.random() * this.state.options.length);
+        const output = this.state.options[randNum];
+        alert(output);
+    };
+    handleAddOption = (option) => {
+        if(!option){
+            return 'Enter valid value to add item'
+        } else if(this.state.options.indexOf(option) > -1 ){
+            return 'This option already exists.'
+        } 
+
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option),
+
+        }));
+    };
+    componentDidMount = () => {
         try{
             const json = localStorage.getItem('options');
             const options = JSON.parse(json);
@@ -37,32 +57,7 @@ class IndecisionApp extends React.Component {
     componentWillUnmount(){
         console.log('componentWillUnmount');
     }
-    handleDeleteOptions(){
-        this.setState(() => ( { options:[] } ));
-    }
-    // Setting it to False removes everything. Setting true, keeps it
-    handleDeleteOption(optionToRemove){
-        this.setState((prevState)=> ({
-            options: prevState.options.filter((option)=> !(optionToRemove === option))
-        }))
-    }
-    handlePick(){
-        const randNum = Math.floor(Math.random() * this.state.options.length);
-        const output = this.state.options[randNum];
-        alert(output);
-    }
-    handleAddOption(option){
-        if(!option){
-            return 'Enter valid value to add item'
-        } else if(this.state.options.indexOf(option) > -1 ){
-            return 'This option already exists.'
-        } 
-
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option),
-
-        }));
-    }
+    
     render() {
 
         const subtitle = 'Put your life in the hands of your computer';
