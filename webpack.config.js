e9eaa18205d28c6cef0e157392c32c567479e626
Delwyn <Delwyn@delwyns-air.lan>
2019-04-23 23:36:00 -0400

    added normalized library to reset settings across various browsers

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,19 @@ module.exports = {
             loader:'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        },{
+            test:/\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }
+        ]
+    },
+    devtool:'cheap-module-eval-source-map',
+    devServer:{
+        contentBase:path.join(__dirname,'public')
     }
 };
 
