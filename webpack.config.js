6afe4e91faada02a5ee0fb006a91bca9cbb95c1d
Delwyn <khmer.delwyn.9523@gmail.com>
2019-11-10 12:26:15 -0500

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,41 @@
-const path = require('path');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-
-module.exports = {
-    entry:'./src/app.js',
-    output:{
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js'
+module.exports = env => {
+    const isProduction = env === 'production'
+ 
+  return {
+    entry: path.join(__dirname, 'src','app.js') , //'./src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
     module: {
-        rules:[{
-            loader:'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
         }
-        ]
+      ]
     },
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:path.join(__dirname,'public')
-    }
-};
+    devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
+    devServer: {
+      host: '192.168.86.235',
+      port:'8080',
+      contentBase: path.join(__dirname, 'public'),
+      publicPath:'/dist/'
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:'styles.css'
+        })
+    ]
+  }
+}
 
 // loader
